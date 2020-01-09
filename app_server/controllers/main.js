47b97a497145a83b8f58bcf0fdc1d2436d501c6e
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-09 19:41:16 -0200

    testing second commit

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,5 @@
 var homePageTemplateView = 'index';
-var homePageDataObject = { title: 'I am an Express app!' };
+var homePageDataObject = { title: 'I am an Express app and I am live at Herokuapp!' };
 var homepageController = function (req, res) { res.render(homePageTemplateView, homePageDataObject); };
 
 module.exports.index = homepageController;
