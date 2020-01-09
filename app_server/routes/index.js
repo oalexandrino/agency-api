121f7511a01851afab8c48aea9c31a8d379ab6e1
olavo.barbosa <olavo.barbosa@ati.pe.gov.br>
2020-01-09 16:10:35 -0300

    adding bootstrap and jquery to the public folder

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
+var mainController = require('../controllers/main');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+/* GET home page. */ 
+router.get('/', mainController.index);
 module.exports = router;
