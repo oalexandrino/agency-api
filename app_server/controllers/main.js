c2e9c990555c874e79a917e047e59e683bb61344
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-10 20:22:16 -0200

    make it better

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,5 @@
 var homePageTemplateView = 'index';
-var homePageDataObject = { title: 'Agency website' };
+var homePageDataObject = { title: 'Agency website: Bootstrap, Express and Node.Js' };
 var homepageController = function (req, res) { res.render(homePageTemplateView, homePageDataObject); };
 
 module.exports.index = homepageController;
