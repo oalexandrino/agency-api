0519c00d0e8d43b631a71ad8079fa364796b2ec6
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-11 12:47:59 -0200

    implementing portfolio array for the portfolio jade include

---
+++ app_server/data/homePageDataObject.js
@@ -0,0 +1,24 @@
+var data = {
+    title: 'Agency website: Bootstrap, Express and Node.Js',
+    portfolios: [{
+        name: 'Kanban Trainning',
+        description: 'This is description of the Kanban Trainning',
+        price: 3,
+        availableDate: '1/30/2021',
+        thumbnail: '01-thumbnail.jpg',
+    }, {
+        name: 'SAFe POPM Trainning',
+        description: 'This is description of the SAFe POPM Trainning',
+        price: 4,
+        availableDate: '1/30/2020',
+        thumbnail: '02-thumbnail.jpg',
+    }, {
+        name: 'Scrum Trainning',
+        description: 'This is description of the Scrum Trainning',
+        price: 4,
+        availableDate: '1/30/2020',
+        thumbnail: '03-thumbnail.jpg',
+    }]
+};
+
+module.exports = data;
