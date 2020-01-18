303c60303aecb173186bd28b978afb441bae33c5
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-18 18:29:25 -0200

    add more methods to team controller

--- app_api/controllers/teamController.js
+++ app_api/controllers/teamController.js
@@ -2,19 +2,43 @@ var mongoose = require('mongoose');
 var TeamModel = mongoose.model('team');
 
 var sendJsonResponse = function (res, err, content) {
-    httpStatus = 200;
+    var httpStatus = 200;
     if (err) {
         content = err;
         httpStatus = 404;
-    } 
-    
+    }
+
     console.log("Result: " + content);
     res.status(httpStatus);
     res.json(content);
+}
+
+module.exports.teamInfo = function (req, res ) {
+    TeamModel.find().exec(function (err, content) {
+        sendJsonResponse(res, err, content);
+    });
+};
+ 
+module.exports.title = function (req, res) {
+    TeamModel.find().select("title").exec(function (err, content) {
+        sendJsonResponse(res, err, content);
+    });
 };
 
-module.exports.teamListing = function (req, res) {
-    TeamModel.find().exec(function (err, content) {
+module.exports.headline = function (req, res) {
+    TeamModel.find().select("headline").exec(function (err, content) {
+        sendJsonResponse(res, err, content);
+    });
+};
+
+module.exports.description = function (req, res) {
+    TeamModel.find().select("description").exec(function (err, content) {
+        sendJsonResponse(res, err, content);
+    });
+};
+
+module.exports.team = function (req, res) {
+    TeamModel.find().select("team").exec(function (err, content) {
         sendJsonResponse(res, err, content);
     });
 };
@@ -22,8 +46,8 @@ module.exports.teamListing = function (req, res) {
 module.exports.getTeamMember = function (req, res) {
 
     TeamModel
-        .find({ "team.email": req.params.email }, {'team.$': 1})
+        .find({ "team.email": req.params.email }, { 'team.$': 1 })
         .exec(function (err, content) {
-               sendJsonResponse(res, err, content);
+            sendJsonResponse(res, err, content);
         });
 };
--- app_api/routes/api_routers.js
+++ app_api/routes/api_routers.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var teamController = require('../controllers/teamController');
 
-router.get('/team', teamController.teamListing);
+router.get('/team/info', teamController.teamInfo);
+router.get('/team/title', teamController.title);
+router.get('/team/headline', teamController.headline);
+router.get('/team/description', teamController.description);
+router.get('/team', teamController.team);
 router.get('/team/:email', teamController.getTeamMember);
 module.exports = router;
