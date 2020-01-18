b3c2657099cd5fcc97746600219fd0d9415c0b8f
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-18 10:56:10 -0200

    refactoring sendJsonResponse

--- app_api/controllers/teamController.js
+++ app_api/controllers/teamController.js
@@ -1,14 +1,21 @@
 var mongoose = require('mongoose');
 var TeamModel = mongoose.model('team');
 
-var sendJsonResponse = function(res, status, content) {
-    res.status(status);
+var sendJsonResponse = function (res, err, content) {
+    httpStatus = 200;
+    if (err) {
+        content = err;
+        httpStatus = 404;
+    } 
+    
+    console.log("Result: " + content);
+    res.status(httpStatus);
     res.json(content);
-  };
+};
 
 module.exports.teamListing = function (req, res) {
-    TeamModel.find().exec(function(err, arrTeamListing) {
-        sendJsonResponse(res, 200, arrTeamListing);
+    TeamModel.find().exec(function (err, content) {
+        sendJsonResponse(res, err, content);
     });
 };
 
@@ -16,13 +23,7 @@ module.exports.getTeamMember = function (req, res) {
 
     TeamModel
         .find({ "team.email": req.params.email }, {'team.$': 1})
-        .exec(function (err, team) {
-            loginfo = team;
-            if (err) {
-                loginfo = err;
-                sendJsonResponse(res, 404, err); 
-            } 
-            console.log(loginfo);
-            sendJsonResponse(res, 200, team);
+        .exec(function (err, content) {
+               sendJsonResponse(res, err, content);
         });
 };
