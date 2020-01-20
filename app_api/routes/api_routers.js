721aaf3e6aa454cebe4d77eadf20ae7fbbecec36
olavo.barbosa <olavo.barbosa@ati.pe.gov.br>
2020-01-20 17:19:58 -0300

    checking if there is a team member with the provided email in addTeamMember controller

--- app_api/routes/api_routers.js
+++ app_api/routes/api_routers.js
@@ -12,9 +12,9 @@ router.get('/service/:idService', serviceController.getService);
 router.get('/team/', teamController.teamInfo);
 router.post('/team/', teamController.addTeamMember);
 router.get('/team/members/', teamController.members);
+router.get('/team/members/:email', teamController.getTeamMember);
 router.get('/team/title', teamController.title);
 router.get('/team/headline', teamController.headline);
 router.get('/team/description', teamController.description);
-router.get('/team/:email', teamController.getTeamMember);
 
 module.exports = router;
