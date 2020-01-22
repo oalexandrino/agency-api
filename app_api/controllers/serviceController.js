b99d97c897349a6505dfe14984b4806263c47776
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-21 22:23:18 -0200

    implementing delete method for serviceController

--- app_api/controllers/serviceController.js
+++ app_api/controllers/serviceController.js
@@ -2,6 +2,23 @@ var mongoose = require('mongoose');
 var ServiceModel = mongoose.model('service');
 var responseUtilities = require("../lib/agency/util/responseUtilities");
 
+module.exports.delete = function (req, res) {
+    
+    var id = req.body.id;
+
+    ServiceModel
+        .findByIdAndDelete({ "_id": id })
+        .exec(function (err, result) {
+            
+            responseUtilities.sendJsonResponse(res, err, { "message": "Service has been removed successful." });
+            
+        });
+};
+
+module.exports.update = function (req, res) {
+    
+};
+
 module.exports.getService = function (req, res) {
     
     ServiceModel.findById(req.params.idService).exec(function (err, content) {
