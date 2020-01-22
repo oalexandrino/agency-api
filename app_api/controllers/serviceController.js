1bfa6a1553e4631ede00cb09dfbb00dee5e11691
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-22 01:25:15 -0200

    evolving servicesController

--- app_api/controllers/serviceController.js
+++ app_api/controllers/serviceController.js
@@ -3,27 +3,63 @@ var ServiceModel = mongoose.model('service');
 var responseUtilities = require("../lib/agency/util/responseUtilities");
 
 module.exports.delete = function (req, res) {
-    
+
     var id = req.body.id;
 
     ServiceModel
-        .findByIdAndDelete({ "_id": id })
+        .findByIdAndDelete({"_id": id})
         .exec(function (err, result) {
+            var message = "Service has been removed successful.";
+            if (!result) {
+                message = "Error at deleting service.";
+            }
             
-            responseUtilities.sendJsonResponse(res, err, { "message": "Service has been removed successful." });
-            
+            responseUtilities.sendJsonResponse(res, err, {"message": message});
+
         });
 };
 
 module.exports.update = function (req, res) {
-    
+
+    var query = { _id: req.body.id };
+    var options = { new: true };
+    var update = {
+        "$set": {
+            "description": req.body.description,
+            "title": req.body.title
+        }
+    };
+
+    ServiceModel.findByIdAndUpdate(query, update, options, function (err, result) {
+        var message = "Service has been updated successful.";
+        if (!result) {
+            message = "Error at updating service.";
+        }
+
+        responseUtilities.sendJsonResponse(res, err, { "message": message });
+    });
+
+
 };
 
 module.exports.getService = function (req, res) {
     
-    ServiceModel.findById(req.params.idService).exec(function (err, content) {
-        responseUtilities.sendJsonResponse(res, err, content);
-    });
+    var valid = mongoose.Types.ObjectId.isValid(req.params.idService);
+    if (valid) {
+        var query = { "_id": req.params.idService };
+        ServiceModel.findById(query).exec(function (err, result) {
+            var data = { "message": "Service not found." };
+            if (!result) {
+                result = data;
+            }
+
+            responseUtilities.sendJsonResponse(res, err, result);
+        });
+    } else {
+        responseUtilities.sendJsonResponse(res, false, { "message": "Service id is not valid." });
+    }    
+    
+
 };
 
 module.exports.serviceListing = function (req, res) {
