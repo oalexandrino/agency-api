cc77353a69ed6f42ede5704db9ecbeaf9549b8ad
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-27 18:03:28 -0200

    removing sensitive data

--- app_api/lib/agency/db/firebase/FirebaseConnection.js
+++ app_api/lib/agency/db/firebase/FirebaseConnection.js
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyADr0EzuAZjfvD34DAV6z5w0N806-pMLPo",
-    authDomain: "agency-website-2b82f.firebaseapp.com",
-    databaseURL: 'https://agency-website-2b82f.firebaseio.com',
-    projectId: "agency-website-2b82f",
-    storageBucket: "agency-website-2b82f.appspot.com",
-    messagingSenderId: "619747259950",
-    appId: "agency-website-2b82f"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 class FirebaseConnection {
