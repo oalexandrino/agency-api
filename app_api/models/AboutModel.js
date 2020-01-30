42ef28b5b24183c89b522b270932ba1d84a99a32
Olavo Alexandrino <oalexandrino@gmail.com>
2020-01-30 21:48:43 -0200

    starting about endpoint

--- app_api/models/AboutModel.js
+++ app_api/models/AboutModel.js
@@ -1,26 +1,25 @@
 var mongoose = require('mongoose');
 
 var aboutModelSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        required: true
+    },
     title: {
         type: String,
         required: true,
-        min: 5,
+        min: 3,
         max: 20
     },
     description: {
         type: String,
         required: true,
-        min: 20,
-        max: 50
-    },
-    Date: {
-        type: Date,
-        "default": Date.now
-    },
-    image: {
-        data: Buffer,
-        contentType: String
-},
+        min: 3,
+        max: 100
+    }
+}, {
+    collection: 'about'
 });
 
-mongoose.model('Services', aboutModelSchema);
+aboutModelSchema.set('collection', 'about');
+mongoose.model('about', aboutModelSchema);
