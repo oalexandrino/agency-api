b58c7f6ac37ce0d1dcbe5099c1e10bae6a58129a
Olavo Alexandrino <oalexandrino@gmail.com>
2020-02-02 15:44:04 -0200

    implementing sending email

--- app_server/routes/server_routers.js
+++ app_server/routes/server_routers.js
@@ -3,8 +3,7 @@ var homePageController = require('../controllers/homePageController');
 var pageNotFoundController = require('../controllers/pageNotFoundController');
 var router = express.Router();
 
-
-/* GET home page. */ 
 router.get('/', homePageController.index);
 router.get('/pageNotFound', pageNotFoundController.index);
+router.post('/sendEmail', homePageController.sendEmail);
 module.exports = router;
