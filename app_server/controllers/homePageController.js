2bb8e2f14346a9ee1c419e55140cd6d907e07a01
Olavo Alexandrino <oalexandrino@gmail.com>
2020-02-02 15:44:04 -0200

    implementing sending email

--- app_server/controllers/homePageController.js
+++ app_server/controllers/homePageController.js
@@ -3,6 +3,7 @@ var ApiConfiguration = require('../../app_api/lib/agency/settings/ApiConfigurati
 var request = require('request');
 var Promise = require("bluebird");
 var request = require('request-promise');
+const nodemailer = require('nodemailer');
 
 var homePageInfo = function (req, res) {
 
@@ -64,4 +65,54 @@ var homePageInfo = function (req, res) {
     });
 };
 
+var sendEmail = function (req, res) {
+    
+    const message = req.body.message;
+    const email = req.body.email;
+    
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_SMTP_PORT,
+        secure: process.env.EMAIL_SECURE,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        },
+        tls: { rejectUnauthorized: process.env.EMAIL_TLS }
+    });
+    
+    const mailOptionsToWebSite = {
+        from: process.env.EMAIL_FROM_EMAIL,
+        to: process.env.EMAIL_FROM_TO,
+        subject: process.env.EMAIL_SUBJECT_WEBSITE,
+        html: message
+    };
+    
+    const mailOptionsToSender = {
+        from: process.env.EMAIL_FROM_EMAIL,
+        to: email,
+        subject: process.env.EMAIL_SUBJECT_SENDER,
+        html: process.env.EMAIL_MESSAGE
+    };
+    
+    transporter.sendMail(mailOptionsToWebSite, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email to website sent: ' + info.response);
+        }
+    });
+    
+    transporter.sendMail(mailOptionsToSender, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email to sender sent: ' + info.response);
+        }
+    });    
+    
+    res.end();
+}
+
 module.exports.index = homePageInfo;
+module.exports.sendEmail = sendEmail;
