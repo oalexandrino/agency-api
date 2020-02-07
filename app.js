5424ab3bae587f83f215683ce3f813ae91b2dc8d
olavo.barbosa <olavo.barbosa@ati.pe.gov.br>
2020-02-07 10:03:33 -0300

    adding CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -24,6 +25,24 @@ var path = require('path');
 
 // express app
 var app = express();
+
+var allowedOrigins = ['http://localhost:3000',
+                      'http://localhost:4200', 'https://agency-angular-express-node.herokuapp.com'];
+
+//Enabling CORS
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // let the app to get access to static folders
 app.use(express.static(path.join(__dirname, 'public')));
