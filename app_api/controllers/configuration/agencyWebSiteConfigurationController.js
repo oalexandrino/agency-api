25ee6e56ad07e918da31a8f4b7a45c9fe6cdbafd
Olavo Alexandrino <oalexandrino@gmail.com>
2020-03-23 15:51:58 -0200

    refactoring config controller

--- app_api/controllers/configuration/agencyWebSiteConfigurationController.js
+++ app_api/controllers/configuration/agencyWebSiteConfigurationController.js
@@ -1,44 +1,62 @@
+/*
+MIT License
+
+Copyright (c) 2020 Olavo Alexandrino, http://www.oalexandrino.com.br
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 var mongoose = require('mongoose');
 var WebsiteModel = mongoose.model('agencyWebsiteConfiguration');
 var responseUtilities = require("../../lib/agency/util/responseUtilities");
 
 module.exports.update = function (req, res) {
 
-    WebsiteModel.find().exec(function (err, content) {
+    const {
+        site_title,
+        site_headline,
+        link_site,
+        service_headline,
+        portfolio_headline,
+        about_headline,
+        contact_headline } = req.body;
 
-        var _id = content[0]._id;
-        var valid = mongoose.Types.ObjectId.isValid(_id);
-
-        if (valid ) {
-
-            var query = {_id: _id};
-            var options = { new: true };
-            var update = {
-                $set: {
-                    _id: _id,
-                    site_title: req.body.site_title,
-                    site_headline: req.body.site_headline,
-                    link_site: req.body.link_site,
-                    service_headline: req.body.service_headline,
-                    portfolio_headline: req.body.portfolio_headline,
-                    about_headline: req.body.about_headline,
-                    contact_headline: req.body.contact_headline
-                }
-            };
-
-            WebsiteModel.findByIdAndUpdate(query, update, options, function (err, result) {
-                var message = "General configurations have been updated successful.";
-                if (!result) {
-                    message = "Error at updating general configurations.";
-                }
-                responseUtilities.sendJSON(res, err, { "message": message });
-            }
-            );
+    var query = {};
+    var update = {
+        $set: {
+            site_title: site_title,
+            site_headline: site_headline,
+            link_site: link_site,
+            service_headline: service_headline,
+            portfolio_headline: portfolio_headline,
+            about_headline: about_headline,
+            contact_headline: contact_headline
         }
-        else {
-            responseUtilities.sendJSON(res, err, { "message": "Error at updating general configurations. Problem at finding the document." });
+    };
+
+    WebsiteModel.updateOne(query, update, function (err, result) {
+        var message = "General configurations have been updated successful.";
+        if (!result) {
+            message = "Error at updating general configurations.";
         }
-    });
+        responseUtilities.sendJSON(res, err, { "message": message });
+    }
+    );
 };
 
 module.exports.configListing = function (req, res) {
